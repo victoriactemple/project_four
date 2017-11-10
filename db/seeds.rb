0d968795cfbb48a4eb30952b386b94556803827f
victoriactemple <victoriactemple@gmail.com>
2017-11-10 15:55:51 -0500

    added 6 parks to seed file, ran seeds, received the seeds in Postman, and added hirb into Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Park.destroy_all
+
+Park.create({name: "Providence Canyon", 
+description: "Providence Canyon State Outdoor Recreation Area is a 1,003 acres state park located in Stewart County in southwest Georgia. The park contains Providence Canyon, which is sometimes called Georgia's 'Little Grand Canyon'.", 
+image: "http://gastateparks.org/sites/default/files/parks/ProvidenceCanyon.jpg",
+address: "8930 Canyon Rd, Lumpkin, GA 31815", lat: 32.0675, 
+lon: -84.9037
+})
+
+Park.create({name: "Sweetwater Creek", 
+description: "Sweetwater Creek State Park is a 2,549 acre Georgia state park located in the New Manchester area of east Douglas County, 15 miles from downtown Atlanta. The park is named after Sweetwater Creek which runs through the park.", 
+image: "https://i.ytimg.com/vi/44kMEO39Bu0/maxresdefault.jpg",
+address: "1750 Mt Vernon Rd, Lithia Springs, GA 30122", lat: 33.7525, 
+lon: -84.6287
+})
+
+
+Park.create({name: "Cloudland Canyon", 
+description: "Cloudland Canyon State Park is in northwest Georgia, on the western edge of Lookout Mountain. Boulder-strewn Sitton Gulch Creek cuts a deep gorge into the mountain. Sitton's Gulch Trail runs parallel to the creek. The short, steep Waterfalls Trail, with stairs down into the canyon, leads to Cherokee Falls and Hemlock Falls, where water cascades down into pools. Nearby, Sitton’s Cave has mineral formations.", 
+image: "http://media-cdn.timesfreepress.com/img/news/tease/2013/05/30/Cloudland_t618_t1070_h04797ef87fd1bbff0de64d8f73a71fae70ac0ad5.jpg",
+address: "122 Cloudland Canyon Park Rd, Rising Fawn, GA 30738", 
+lat: 34.8395, 
+lon: -85.4840
+})
+
+
+Park.create({name: "Panola Mountain", 
+description: "Just 15 minutes from Atlanta is a recreational haven for all ages and interests. Visitors can picnic while children work off energy on the playground. The park offers archery, boat rental, geocaching, orienteering, birding and tree-climbing programs. A paved trail is open for biking, roller blading, jogging and dog-walking, while forested fitness trails are open for hiking and running.", 
+image: "https://cdn-assets.alltrails.com/uploads/photo/image/11167434/extra_large_3c714b9c2d3f19823ee8a20f172885f9.jpg",
+address: "2620 Highway 155 SW
+Stockbridge, GA 30281
+Henry County", 
+lat: 33.6250, 
+lon: -84.1715
+})
+
+
+Park.create({name: "Vogel", 
+description: "Vogel State Park is a 233-acre or 94 hectares state park located at the base of Blood Mountain in the Chattahoochee National Forest. It became one of the first two parks in Georgia when it founded a state park system in 1931", 
+image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/vogel-state-park-color-robert-walters.jpg",
+address: "405 Vogel State Park Rd, Blairsville, GA 30512", 
+lat: 34.7649, 
+lon: -83.9255
+})
+
+
+
+Park.create({name: "Tallulah Gorge", 
+description: "One of the most spectacular canyons in the eastern U.S., Tallulah Gorge is two miles long and nearly 1,000 feet deep. Visitors can hike rim trails to several overlooks, or they can obtain a permit to hike to the gorge floor (100 per day, not available during water releases). A suspension bridge sways 80 feet above the rocky bottom, providing spectacular views of the river and waterfalls. Tightrope walkers have twice crossed the gorge, and visitors can still see towers used by Karl Wallenda. A paved path follows an on old railroad bed, perfect for strollers and bicycles, while mountain bikers can test their skills on a challenging 10 mile trail.", 
+image: "http://gastateparks.org/sites/default/files/parks/TallulahGorge.jpg",
+address: "338 Jane Hurt Yarn Drive
+Tallulah Falls, GA 30573
+Rabun County", 
+lat: 34.7335, 
+lon: -83.3715
+})
+
+
+
+
