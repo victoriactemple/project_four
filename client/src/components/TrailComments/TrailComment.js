5a7f56d5d267768fdc87ae1bf6bd13e68c2f70b3
victoriactemple <victoriactemple@gmail.com>
2017-11-13 22:35:28 -0500

    added time display to comments

--- client/src/components/TrailComments/TrailComment.js
+++ client/src/components/TrailComments/TrailComment.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 const TrailComment = (props) => {
     return (
         <div>
         <h3>{props.title}</h3>
         <p>{props.content}</p>
+        <p>posted: <Moment fromNow>{props.created_at}</Moment></p>
 
         </div>
     );
