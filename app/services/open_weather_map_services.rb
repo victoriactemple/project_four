9b6160079080973893dbcb1723add50b564bea56
victoriactemple <victoriactemple@gmail.com>
2017-11-14 20:37:05 -0500

    added imperial unit to API address so that it would render fahrenheit

--- app/services/open_weather_map_services.rb
+++ app/services/open_weather_map_services.rb
@@ -6,7 +6,7 @@ module Services
         def self.get_weather(zipcode)
          @access_api_key = ENV['MY_API_KEY']            
 
-         @response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{zipcode},us&appid=#{@access_api_key}")
+         @response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{zipcode},us&units=imperial&appid=#{@access_api_key}")
          @response
         end
 
