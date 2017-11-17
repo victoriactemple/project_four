bcb1d6446537c0c41335395c0e0907727c8dbe57
victoriactemple <victoriactemple@gmail.com>
2017-11-17 12:52:15 -0500

    added styling to signup form

--- client/src/components/SignUpLogIn.js
+++ client/src/components/SignUpLogIn.js
@@ -3,7 +3,45 @@ import { Redirect } from 'react-router-dom'
 import styled from 'styled-components';
 
 const SignUpLogInPageContainer = styled.div`
+text-align: center;
 min-height: 100vh;
+margin: 0 auto;
+padding-top: 100px;
+
+`
+
+const Button = styled.button`
+cursor: pointer;
+color: black;
+font-size: 1em;
+margin: 1em;
+padding: 0.25em 1em;
+border: 2px solid black;
+border-radius: 3px;
+&:hover{
+    box-shadow: 1px 1px 2px;
+}
+&:focus{
+    outline: none;
+}
+`
+
+const Input = styled.input.attrs({
+    
+	margin: props => props.size || '1em',
+	padding: props => props.size || '2em'
+})`
+	color: black;
+    font-family: "Oxygen", sans-serif;
+	font-size: em;
+	border: 2px solid ;
+	border-radius: 3px;
+
+    margin: ${props => props.margin};
+    padding: ${props => props.padding};
+    &:focus{
+        outline: none;
+    }
 `
 
 
@@ -57,19 +95,19 @@ class SignUpLogIn extends Component {
                     <form>
                         <div>
                             <label htmlFor="email">Email: </label>
-                            <input onChange={this.handleChange} type="text" name="email" value={this.state.email}/>
+                            <Input onChange={this.handleChange} type="text" name="email" value={this.state.email}/>
                         </div>
                         <div>
                             <label htmlFor="password">Password: </label>
-                            <input onChange={this.handleChange} type="password" name="password" value={this.state.password}/>
+                            <Input onChange={this.handleChange} type="password" name="password" value={this.state.password}/>
                         </div>
                         <div>
                             <label htmlFor="password">Confirm Password: </label>
-                            <input onChange={this.handleChange} type="password" name="password_confirmation" value={this.state.password_confirmation}/>
+                            <Input onChange={this.handleChange} type="password" name="password_confirmation" value={this.state.password_confirmation}/>
                         </div>
 
-                        <button onClick={this.signUp}>SignUp</button>
-                        <button onClick={this.signIn}>Login</button>
+                        <Button onClick={this.signUp}>SignUp</Button>
+                        <Button onClick={this.signIn}>Login</Button>
                     </form>
             </SignUpLogInPageContainer>
         );
